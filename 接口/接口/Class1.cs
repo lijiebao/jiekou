ea1c8537258b3b0bf4a3d89768a8503c35752162
lijiebao <lijiebao@hengtiansoft.com>
2017-04-19 17:22:16 +0800

    接口

---
+++ 接口/接口/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 接口
+{
+    class Program
+    {
+        interface ImyInterfance
+    {
+        string ID
+        {
+            get;
+            set;
+        }
+        string Name
+        {
+            get;
+            set;
+        }
+        void ShowInfo();
+       {
+        Console.WriteLine("编号\t 姓名");
+        Console.WriteLine(ID+"\t"+name);
+       }
+    }
+
+    }
+}
---
+++ 接口/接口/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using 接口;
+
+namespace 接口
+{
+    class Program : ImyIneterface
+    {
+        string id = "";
+        string name = "";
+        public string ID
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("编号\t 姓名");
+            Console.WriteLine(ID + "\t" + name);
+        }
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            ImyInterfance imyIneterface = program;
+            imyIneterface.ID = "TMD";
+            imyIneterface.Name = "放开那个女孩，让我来";
+            imyIneterface.ShowInfo();
+
+        }
+    }
+}
+
